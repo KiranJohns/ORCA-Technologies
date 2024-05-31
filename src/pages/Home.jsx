88b4321f5a5a639866c3bf17a97886ede30dbcff
KiranJohns <mekiranjohnson@gmail.com>
2024-06-01 02:09:41 +0530

    island mq adjust

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
     let rotation = [0.1, 4.7, 0];
 
     if (window.innerWidth < 768) {
-      screenScale = [0.75, 0.75, 0.75];
+      screenScale = [0.50, 0.50, 0.50];
     } else {
       screenScale = [1, 1, 1];
     }
@@ -41,7 +41,7 @@ const Home = () => {
     let screenScale, screenPosition;
 
     if (window.innerWidth < 768) {
-      screenScale = [1.5, 1.5, 1.5];
+      screenScale = [1, 1, 1];
       screenPosition = [0, -1.5, 0];
     } else {
       screenScale = [3, 3, 3];
